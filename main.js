38241ed96bc953e5186ead16d9f96e8a596c8ae7
gapa-pakar <gapa.rescue@gmail.com>
2024-07-03 08:07:10 +0300

    styling

--- main.js
+++ main.js
@@ -381,7 +381,7 @@ var page5 = () => {
             li.innerText = `${page5Content[i]}`;
             document.getElementById("sortlist").appendChild(li);
         }
-        // document.getElementById("arrows").style.visibility = "hidden";
+        document.getElementById("arrows").style.visibility = "hidden";
         page5Once = 1;
     }
     
